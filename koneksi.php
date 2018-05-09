e48d1231168dbd994f27b1208f8b601021aaf4f8
afimakarim <afi.makarim12@gmail.com>
2018-05-09 16:36:05 +0700

    update koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
 class Koneksi {
     private $server = "localhost";
-    private $username = "root";//root
-    private $password = "";//""
-    private $db = "phb";
+    private $username = "id4993016_afi";//root
+    private $password = "kaligangsa";//""
+    private $db = "id4993016_stock_barang";
 
     function getKoneksi(){
         return new mysqli($this->server, $this->username,$this->password, $this->db);
